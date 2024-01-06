f48e19f8586e4dc6acec566fdfa7b8154395f185
Simone Dalla <simodalla@gmail.com>
2024-01-06 20:17:12 +0100

    Party New page added

--- party/views/__init__.py
+++ party/views/__init__.py
@@ -1,4 +1,16 @@
+from .new_party_views import (
+    page_new_party,
+    partial_check_invitation,
+    partial_check_party_date,
+)
 from .party_details_views import PartyDetailPage, PartyDetailPartial
 from .party_list_views import PartyListPage
 
-__all__ = ["PartyListPage", "PartyDetailPage", "PartyDetailPartial"]
+__all__ = [
+    "PartyListPage",
+    "PartyDetailPage",
+    "PartyDetailPartial",
+    "page_new_party",
+    "partial_check_party_date",
+    "partial_check_invitation",
+]
